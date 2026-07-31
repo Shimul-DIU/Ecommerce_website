8aaec01e7f17c9284528ec0a1e20f76a05b0f542
MD.Shimul <md.shimuldiu@gmail.com>
2026-08-01 01:40:40 +0600

    fix: add file extension to import statement in clientOrderRoute

--- server/routes/orderRoute/clientOrderRoute.js
+++ server/routes/orderRoute/clientOrderRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createOrder } from '../../controller/orderController'
+import { createOrder } from '../../controller/orderController.js'
 const orderRouter=express.Router()
 orderRouter.post('/', createOrder)
 
