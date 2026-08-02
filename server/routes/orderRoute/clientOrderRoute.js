4519a3f434f9267fbcd63af1d1ba015ca9952b38
MD.Shimul <md.shimuldiu@gmail.com>
2026-08-03 00:08:10 +0600

    fix: import missing ordersDetails controller in order routes

--- server/routes/orderRoute/clientOrderRoute.js
+++ server/routes/orderRoute/clientOrderRoute.js
@@ -1,9 +1,11 @@
-import express from 'express'
-import { createOrder } from '../../controller/orderController.js'
-import ordersDetails from '../../controller/orders/OrdersDetails.js'
-import passport from 'passport'
-const orderRouter=express.Router()
-orderRouter.post('/', passport.authenticate("jwt", { session: false }), createOrder)
+import express from 'express';
+import { createOrder } from '../../controller/orderController.js';
+import ordersDetails from '../../controller/orders/OrdersDetails.js';
+import passport from 'passport';
 
-orderRouter.get('/order-details', passport.authenticate('jwt', { session: false }),ordersDetails)
-export default orderRouter
+const orderRouter = express.Router();
+
+orderRouter.post('/', passport.authenticate("jwt", { session: false }), createOrder);
+orderRouter.get('/order-details', passport.authenticate('jwt', { session: false }), ordersDetails);
+
+export default orderRouter;
